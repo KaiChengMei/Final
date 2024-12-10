407a13e142892d1fcaf5bb35c355de5a09e73004
Kai-Cheng Mei <kennymei47@gmail.com>
2024-12-09 19:30:11 -0800

    update6

--- main.cpp
+++ main.cpp
@@ -2,26 +2,26 @@
 #include <string>
 using namespace std;
 
-struct Custumornode {
+struct Customernode {
         string name;
         string order;
         Customernode* next;
-    }
+    };
 
 // 10 name
-string name[] = {"Frank","Alex","Kai","Kenny","Zoe","Stanley","Timothy","Renee","Nina","Jinx"}
-string coffeeorder[] = {"Latte","Expresso","Mocha","Water"}
+string names[] = {"Frank","Alex","Kai","Kenny","Zoe","Stanley","Timothy","Renee","Nina","Jinx"};
+string coffeeorder[] = {"Latte","Expresso","Mocha","Water"};
 
-void coffeeaddqueue(Customernode head, string name, string coffeeorder) {
-    Customernode* newnode = new Customernode{name, order, nullptr};
-    CustomerNode* temp = head;
+void coffeeaddqueue(Customernode* head, string name, string coffeeorder) {
+    Customernode* newnode = new Customernode{name, coffeeorder, nullptr};
+    Customernode* temp = head;
     temp = temp->next;
     temp->next = newnode;
 }
 
-void servecoffee(Customernode head) {
+void servecoffee(Customernode* head) {
     // print the person served
-    cout <<
+    cout << "Coffee Serving: " << head->name << "with drik: " << head ->order << endl;
 
     if (head) {
         head = head->next;
@@ -29,24 +29,22 @@ void servecoffee(Customernode head) {
         cout << "There is no one Coffe booth " << endl;
     }
 }
-void joincoffee(Customernode head, string name, string coffeeorder) {
 
-} 
 
 int main() {
 
     Customernode* cofffeeQ;
 
-    for ( i = 0; i < 3; i++) {
-        cout << "Initial move: "
-        addqueue(cofffeeQ, name[rand % 10], coffeeorder[rand % 4]);
+    for ( int i = 0; i < 3; i++) {
+        cout << "Initial move: ";
+        coffeeaddqueue(cofffeeQ, names[rand() % 10], coffeeorder[rand() % 4]);
     }
 
-    for ( j = 0; j < 10; j++) {
-        cout << "Time: " << i+1 << "CoffeeBooth" << endl;
+    for ( int j = 0; j < 10; j++) {
+        cout << "Time: " << j+1 << "CoffeeBooth" << endl;
         servecoffee(cofffeeQ);
-        if (rand % 2 == 0) {
-            joincoffee(cofffeeQ, name[rand % 10], coffeeorder[rand % 4]);
+        if (rand() % 2 == 0) {
+            coffeeaddqueue(cofffeeQ, names[rand() % 10], coffeeorder[rand() % 4]);
         } 
-
+    }
 }
