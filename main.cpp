5e8675865292370fc49cb10dec4d4fb8420c19b2
Kai-Cheng Mei <kennymei47@gmail.com>
2024-12-09 18:51:09 -0800

    update2

--- main.cpp
+++ main.cpp
@@ -2,11 +2,19 @@
 #include <string>
 using namespace std;
 
+void addqueue() {
+
+}
 
 int main() {
     struct custumor {
         string name;
-        
+        string order;
+        Customernode* next;
+    }
+
+    for ( i = 0; i < 3; i++) {
+        addqueue();
     }
 
 }
