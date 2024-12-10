232956bf58c144a6e6ad4145138f9882f3be53e6
Kai-Cheng Mei <kennymei47@gmail.com>
2024-12-09 19:11:34 -0800

    update4

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-struct custumor {
+struct Custumornode {
         string name;
         string order;
         Customernode* next;
@@ -12,10 +12,19 @@ struct custumor {
 string name[] = {"Frank","Alex","Kai","Kenny","Zoe","Stanley","Timothy","Renee","Nina","Jinx"}
 string coffeeorder[] = {"Latte","Expresso","Mocha","Water"}
 
-void coffeeaddqueue(Customernode head, string name) {
-
+void coffeeaddqueue(Customernode head, string name, string coffeeorder) {
+    Customernode* newnode = new Customernode{name, order, nullptr};
+    CustomerNode* temp = head;
+    temp = temp->next;
+    temp->next = newnode;
 }
 
+void servecoffee(Customernode head, string name, string coffeeorder) {
+    if (head) {
+        head = head->next;
+    } else {
+        cout << "There is no one Coffe booth " << endl;
+    }
 
 void simulationcoffee() {
 
@@ -26,7 +35,7 @@ int main() {
     Customernode* cofffeeQ;
 
     for ( i = 0; i < 3; i++) {
-        addqueue(cofffeeQ, name[rand % 10]);
+        addqueue(cofffeeQ, name[rand % 10], coffeeorder[rand % 4]);
     }
 
 }
