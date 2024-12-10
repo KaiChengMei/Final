12771c0de206d86d553d3c408bbd78562f9c860b
Kai-Cheng Mei <kennymei47@gmail.com>
2024-12-09 19:59:03 -0800

    update9

--- main.cpp
+++ main.cpp
@@ -50,27 +50,51 @@ void servecoffee(Customernode*& head) {
     }
 }
 
+void servemuffin(depue<Customer> queue) {
+    if (!queue.empty()) {
+        cout << "Muffin Serving:" << queue.front;
+        queue.popfont();
+    } else {
+        cout << "There is no one Muffin booth " << endl;
+    }
+}
+
+void servebracelet(vector<Customer> queue) {
+
+}
+
+void serveicecream(queue<Customer> queue) {
+
+}
 
 int main() {
 
     srand(time(0));
 
     Customernode* cofffeeQ = nullptr;
-    depue(Customer) muffinQ;
-    vector(Customer) braceletQ;
-    queue(Customer) icecreamQ;
+    depue<Customer> muffinQ;
+    vector<Customer> braceletQ;
+    queue<Customer> icecreamQ;
 
     cout << "Initial move: ";
     for ( int i = 0; i < 3; i++) {
         coffeeaddqueue(cofffeeQ, names[rand() % 10], coffeeorder[rand() % 4]);
-        muffinQ.push_back(names[rand() % 10], coffeeorder[rand() % 4]);
-        braceletQ.push_back(names[rand() % 10], coffeeorder[rand() % 4]);
-        icecreamQ.push_back(names[rand() % 10], coffeeorder[rand() % 4]);
+        muffinQ.push_back(names[rand() % 10], muffinorder[rand() % 4]);
+        braceletQ.push_back(names[rand() % 10], braceletorder[rand() % 4]);
+        icecreamQ.push_back(names[rand() % 10], icecreamorder[rand() % 4]);
     }
     cout << endl;
     for ( int j = 0; j < 10; j++) {
-        cout << "Time: " << j+1 << " CoffeeBooth" << endl;
+        cout << "Time: " << j+1;
+
+        cout << " CoffeeBooth" << endl;
         servecoffee(cofffeeQ);
+        if (rand() % 2 == 0) {
+            coffeeaddqueue(cofffeeQ, names[rand() % 10], coffeeorder[rand() % 4]);
+        } 
+
+        cout << " MuffinBooth" << endl;
+        serveMuffin(muffinQ);
         if (rand() % 2 == 0) {
             coffeeaddqueue(cofffeeQ, names[rand() % 10], coffeeorder[rand() % 4]);
         } 
